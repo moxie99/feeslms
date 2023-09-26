a7e39ab2e6e57bd268ec6454aaf0096a9a1f715c
Adeolu David <adeolusegun99@gmail.com>
2023-09-26 23:12:13 +0100

    generated database and pushed to mongodb database

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export const Logo = () => {
-  return <Image height={125} width={125} alt='logo' src='/logo.jpg' />;
+  return <Image height={130} width={130} alt='logo' src='/logo.jpg' />;
 };
