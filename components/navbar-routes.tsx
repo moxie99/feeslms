80248a62d9d7da10b5621ed7297b3ee1610b1732
Adeolu David <adeolusegun99@gmail.com>
2023-10-02 11:05:02 +0100

    changed exit in teachers mode to back

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -30,7 +30,7 @@ export const NavbarRoutes = () => {
           <Link href='/'>
             <Button size='sm' variant='ghost'>
               <LogOut className='h-4 w-4 mr-2' />
-              Exit
+              Back
             </Button>
           </Link>
         ) : isTeacher(userId) ? (
