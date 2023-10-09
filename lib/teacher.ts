84772b97bab9d6d8580c956da72c2bbf5db7640a
Adeolu David <adeolusegun99@gmail.com>
2023-10-09 15:52:56 +0100

    added the fourth and fifth facilitator to access course creation

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,3 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID || userId === process.env.NEXT_PUBLIC_TEACHER_ID_2 || process.env.NEXT_PUBLIC_TEACHER_ID_3;
+  return userId === process.env.NEXT_PUBLIC_TEACHER_ID || userId === process.env.NEXT_PUBLIC_TEACHER_ID_2 || process.env.NEXT_PUBLIC_TEACHER_ID_3 || process.env.NEXT_PUBLIC_TEACHER_ID_4 || process.env.NEXT_PUBLIC_TEACHER_ID_5;
 };
