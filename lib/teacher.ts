03f7b4c3f42db175239f9e2daea74b265933e237
Adeolu David <adeolusegun99@gmail.com>
2023-10-09 21:19:23 +0100

    added a single teacher

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,8 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID || userId === process.env.NEXT_PUBLIC_TEACHER_ID_2 || process.env.NEXT_PUBLIC_TEACHER_ID_3 || process.env.NEXT_PUBLIC_TEACHER_ID_4 || process.env.NEXT_PUBLIC_TEACHER_ID_5 || process.env.NEXT_PUBLIC_TEACHER_ID_6 || process.env.NEXT_PUBLIC_TEACHER_ID_7 || process.env.NEXT_PUBLIC_TEACHER_ID_8;
+  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
 };
+
+
+// {
+//   || userId === process.env.NEXT_PUBLIC_TEACHER_ID_2 || process.env.NEXT_PUBLIC_TEACHER_ID_3 || process.env.NEXT_PUBLIC_TEACHER_ID_4 || process.env.NEXT_PUBLIC_TEACHER_ID_5 || process.env.NEXT_PUBLIC_TEACHER_ID_6 || process.env.NEXT_PUBLIC_TEACHER_ID_7 || process.env.NEXT_PUBLIC_TEACHER_ID_8;
+// }
