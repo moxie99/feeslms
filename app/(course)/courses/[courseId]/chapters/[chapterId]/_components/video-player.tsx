6ca86f3e751d1b06208fda45b1e840fd4f81133b
Adeolu David <adeolusegun99@gmail.com>
2024-04-25 09:29:12 +0100

    edited a chapter folder to enforce redeployment after e.env change

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -68,7 +68,7 @@ export const VideoPlayer = ({
       {isLocked && (
         <div className='absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary'>
           <Lock className='h-8 w-8' />
-          <p className='text-sm'>This chapter is locked.</p>
+          <p className='text-sm'>This chapter is locked. Please, enrol to start watching.</p>
         </div>
       )}
       {!isLocked && (
