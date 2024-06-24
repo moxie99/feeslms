17270b425bdfb6caeb2b8e89e69f24baf7abce41
Adeolu David <adeolusegun99@gmail.com>
2024-06-24 10:48:46 +0100

    added email to Navbarroutes

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -3,6 +3,7 @@ import { Chapter, Course, UserProgress } from '@prisma/client';
 import { NavbarRoutes } from '@/components/navbar-routes';
 
 import { CourseMobileSidebar } from './course-mobile-sidebar';
+import { currentUser } from '@clerk/nextjs';
 
 interface CourseNavbarProps {
   course: Course & {
@@ -13,11 +14,15 @@ interface CourseNavbarProps {
   progressCount: number;
 }
 
-export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
+export const CourseNavbar = async ({
+  course,
+  progressCount,
+}: CourseNavbarProps) => {
+  const user = await currentUser();
   return (
     <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
       <CourseMobileSidebar course={course} progressCount={progressCount} />
-      <NavbarRoutes />
+      <NavbarRoutes email={user?.emailAddresses[0].emailAddress} />
     </div>
   );
 };
